33c8e25c2406f7d9530f8b30ed3a5b2699357950
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-10-30 09:40:59 +0400

    exercise 4

---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,12 @@
+fruits=['Apple','Mango','Orange']
+
+if 'Apple' in fruits:
+ print("I like Apples")
+if 'Grapes' in fruits:
+ print("I like Grapes")
+if 'Orange' in fruits:
+ print("I like orange")
+if 'Avocado' in fruits:
+ print("I like Avocado")
+if 'Pomegranate' in fruits:
+ print(" I like Pomegranate")
