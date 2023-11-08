a088444e09d6b74458fad40079f6076237e5c688
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:12:36 +0400

    Update exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -1,3 +1,5 @@
+# This is a Python program that prints the lyrics of the nursery rhyme
+# "Twinkle, Twinkle, Little Star" with proper formatting and indentation.
 print("""Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
