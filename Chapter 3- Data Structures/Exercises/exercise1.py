432342a234c39fe636e2e154b4bcc8f6a5eff8f0
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:25:16 +0400

    Update exercise1.py

--- Chapter 3- Data Structures/Exercises/exercise1.py
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -1,5 +1,14 @@
-names=['Shaan','Jose','Baiju','Joseph']
+# Create a list called 'names' containing four elements.
+names = ['Shaan', 'Jose', 'Baiju', 'Joseph']
+
+# Print the first element of the 'names' list, which is 'Shaan'.
 print(names[0])
+
+# Print the second element of the 'names' list, which is 'Jose'.
 print(names[1])
+
+# Print the third element of the 'names' list, which is 'Baiju'.
 print(names[2])
+
+# Print the fourth element of the 'names' list, which is 'Joseph'.
 print(names[3])
