2471ee3f7b9ead1befdc066df992771465f7d3c8
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:41:49 +0400

    Update exercise1.py

--- Chapter 6- Loops/Exercises/exercise1.py
+++ Chapter 6- Loops/Exercises/exercise1.py
@@ -1,9 +1,15 @@
+# Set up a prompt asking the user for pizza toppings and how to exit the loop.
 prompt = "\nWhat type of toppings would you like on your pizza?"
 prompt += "\nEnter 'quit' when you are finished: "
 
+# Start an indefinite while loop, which will continue until 'break' is executed.
 while True:
+    # Prompt the user for a pizza topping.
     topping = input(prompt)
+    
+    # Check if the user input is not 'quit'; if not, print the chosen topping and continue the loop.
     if topping != 'quit':
-        print(f"  Im adding {topping} to your pizza.")
+        print(f"  I'm adding {topping} to your pizza.")
+    # If the user inputs 'quit', break out of the loop to exit.
     else:
         break
