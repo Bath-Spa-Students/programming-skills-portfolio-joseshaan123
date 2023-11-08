db66804b3cd07a21515f6095474ca2e2a7eaf9bf
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:29:16 +0400

    Update exercise4.py

--- Chapter 3- Data Structures/Exercises/exercise4.py
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -1,7 +1,16 @@
-family=['Shaan','Baiju','Grace']
-msg=("I'd like to invite ")+family[0].title()+(", to dinner")
+# Create a list called 'family' containing three names.
+family = ['Shaan', 'Baiju', 'Grace']
+
+# Create a message that combines "I'd like to invite " with the first name from the 'family' list (capitalized using the title() method) and "to dinner".
+msg = "I'd like to invite " + family[0].title() + ", to dinner"
+
+# Print the message, inviting the first person in the 'family' list to dinner.
 print(msg)
-msg=("I'd like to invite ")+family[1].title()+(", to dinner")
+
+# Repeat the process for the second person in the list, creating a message and printing it.
+msg = "I'd like to invite " + family[1].title() + ", to dinner"
 print(msg)
-msg=("I'd like to invite ")+family[2].title()+(", to dinner")
+
+# Repeat the process for the third person in the list, creating a message and printing it.
+msg = "I'd like to invite " + family[2].title() + ", to dinner"
 print(msg)
