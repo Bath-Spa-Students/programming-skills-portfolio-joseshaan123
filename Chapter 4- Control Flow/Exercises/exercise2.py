8726b6de19ba1b2bbdf3336598d6fc7111d8e840
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:33:24 +0400

    Update exercise2.py

--- Chapter 4- Control Flow/Exercises/exercise2.py
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -1,12 +1,18 @@
+# Set the variable 'alien_color' to 'green'.
 alien_color = 'green'
 
+# Check if 'alien_color' is 'green' and print a message with 5 points if it is.
+# If it's not 'green,' print a message with 10 points.
 if alien_color == 'green':
     print("You just earned 5 points!")
 else:
     print("You just earned 10 points!")
 
+# Update the 'alien_color' variable to 'yellow'.
 alien_color = 'yellow'
 
+# Check if 'alien_color' is 'green' (which it's not) and print a message with 10 points.
+# Since it's not 'green,' the else block is executed.
 if alien_color == 'green':
     print("You just earned 5 points!")
 else:
