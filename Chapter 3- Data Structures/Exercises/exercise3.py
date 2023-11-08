e8f5f14e3bb8c1285d329d8a055d4d2f4d40617e
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:26:42 +0400

    Update exercise3.py

--- Chapter 3- Data Structures/Exercises/exercise3.py
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -1,9 +1,15 @@
-cars=['Honda','Ford','Toyota','lamborghini']
-statement=("I would like to own a ") + cars[0].title()
+# Create a list called 'cars' containing four car brands.
+cars = ['Honda', 'Ford', 'Toyota', 'lamborghini']
+# Create a statement that combines "I would like to own a " with the first car brand from the 'cars' list (capitalized using the title() method).
+statement = "I would like to own a " + cars[0].title()
+# Print the statement, indicating a desire to own the first car in the list.
 print(statement)
-statement=("I would like to own a ") + cars[1].title()
+# Repeat the process for the second car in the list, creating a statement and printing it.
+statement = "I would like to own a " + cars[1].title()
 print(statement)
-statement=("I would like to own a ") + cars[2].title()
+# Repeat the process for the third car in the list, creating a statement and printing it.
+statement = "I would like to own a " + cars[2].title()
 print(statement)
-statement=("I would like to own a ") + cars[3].title()
+# Repeat the process for the fourth car in the list, creating a statement and printing it.
+statement = "I would like to own a " + cars[3].title()
 print(statement)
