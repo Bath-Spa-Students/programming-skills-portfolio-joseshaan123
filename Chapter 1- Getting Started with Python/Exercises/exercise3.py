6e9d7d6e89aeded3b21c7050fa51b3a8b9be3d56
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:21:04 +0400

    Update exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise3.py
@@ -1,2 +1,5 @@
+# Import the 'datetime' module, which provides classes for working with dates and times.
 import datetime
+# Use the 'datetime.now()' method to get the current date and time.
+# Then, print the current date and time to the console.
 print(datetime.datetime.now())
