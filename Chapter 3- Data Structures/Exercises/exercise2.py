03eab493019b6a1a22c294b279583d4602fd31cc
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:26:11 +0400

    Update exercise2.py

--- Chapter 3- Data Structures/Exercises/exercise2.py
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -1,9 +1,15 @@
-names=['Shaan','jose','Baiju','Joseph']
-msg="hello,"+ names[0].title()+"!"
+# Create a list called 'names' containing four elements.
+names = ['Shaan', 'jose', 'Baiju', 'Joseph']
+# Create a message by combining "hello," with the first name from the 'names' list (capitalized using the title() method).
+msg = "hello," + names[0].title() + "!"
+# Print the message, which will greet the first person in the list with a capitalized name.
 print(msg)
-msg="hello,"+ names[1].title()+"!"
+# Repeat the process for the second person in the list, creating a message and printing it.
+msg = "hello," + names[1].title() + "!"
 print(msg)
-msg="hello,"+ names[2].title()+"!"
+# Repeat the process for the third person in the list, creating a message and printing it.
+msg = "hello," + names[2].title() + "!"
 print(msg)
-msg="hello,"+ names[3].title()+"!"
+# Repeat the process for the fourth person in the list, creating a message and printing it.
+msg = "hello," + names[3].title() + "!"
 print(msg)
