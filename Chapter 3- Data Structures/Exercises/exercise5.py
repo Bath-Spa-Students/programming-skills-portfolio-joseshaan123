9355ce7d2a656bcd48d5c96022f0383bdd3dc3c4
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:30:14 +0400

    Update exercise5.py

--- Chapter 3- Data Structures/Exercises/exercise5.py
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -1,20 +1,28 @@
-family=['Shaan','Baiju','Grace']
-msg=("I'd like to invite ")+family[0].title()+(", to dinner")
+# Create a list called 'family' containing three names.
+family = ['Shaan', 'Baiju', 'Grace']
+# Create a message that combines "I'd like to invite " with the first name from the 'family' list (capitalized using the title() method) and "to dinner".
+msg = "I'd like to invite " + family[0].title() + ", to dinner"
+# Print the message, inviting the first person in the 'family' list to dinner.
 print(msg)
-msg=("I'd like to invite ")+family[1].title()+(", to dinner")
+# Repeat the process for the second person in the list, creating a message and printing it.
+msg = "I'd like to invite " + family[1].title() + ", to dinner"
 print(msg)
-msg=("I'd like to invite ")+family[2].title()+(", to dinner")
+# Repeat the process for the third person in the list, creating a message and printing it.
+msg = "I'd like to invite " + family[2].title() + ", to dinner"
 print(msg)
-
-msg=("Sorry,")+family[1].title()+(" cannot come for dinner")
+# Create a message indicating that the second person in the list cannot come to dinner.
+msg = "Sorry, " + family[1].title() + " cannot come for dinner"
+# Print the message, informing that the second person can't attend dinner.
 print(msg)
-
-del(family[1])
-family.insert(1,'eva')
-
-msg=("I'd like to invite ")+family[0].title()+(", to dinner")
+# Delete the second person from the 'family' list and replace them with 'eva'.
+del family[1]
+family.insert(1, 'eva')
+# Create a message inviting the first person in the modified list to dinner and print it.
+msg = "I'd like to invite " + family[0].title() + ", to dinner"
 print(msg)
-msg=("I'd like to invite ")+family[1].title()+(", to dinner")
+# Create a message inviting the second person (now 'eva') to dinner and print it.
+msg = "I'd like to invite " + family[1].title() + ", to dinner"
 print(msg)
-msg=("I'd like to invite ")+family[2].title()+(", to dinner")
+# Create a message inviting the third person to dinner and print it.
+msg = "I'd like to invite " + family[2].title() + ", to dinner"
 print(msg)
