c8f13bb175290d2a5848fc4e6a80a3aa278b61d2
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:42:32 +0400

    Update exercise2.py

--- Chapter 6- Loops/Exercises/exercise2.py
+++ Chapter 6- Loops/Exercises/exercise2.py
@@ -1,12 +1,20 @@
-ticket= "\nHow old are you?"
+# Set up a prompt asking the user for their age and how to exit the loop.
+ticket = "\nHow old are you?"
 ticket += "\nEnter 'quit' when you are finished. "
 
+# Start an indefinite while loop, which will continue until 'break' is executed.
 while True:
+    # Prompt the user for their age.
     age = input(ticket)
+    
+    # Check if the user input is 'quit'; if so, break out of the loop to exit.
     if age == 'quit':
         break
+
+    # Convert the user's age input to an integer for comparison.
     age = int(age)
 
+    # Check the age and print the appropriate ticket price.
     if age < 3:
         print("  You are free of cost!")
     elif age < 13:
