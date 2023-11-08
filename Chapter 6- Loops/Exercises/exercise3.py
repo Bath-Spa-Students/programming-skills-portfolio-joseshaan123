ab10971f513c02135a2019a6baad197cac985a55
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:42:59 +0400

    Update exercise3.py

--- Chapter 6- Loops/Exercises/exercise3.py
+++ Chapter 6- Loops/Exercises/exercise3.py
@@ -1,3 +1,6 @@
+# Initialize a variable 'a' to 1 and start an infinite while loop.
+# Inside the loop, print the current value of 'a' and increment it by 1 in each iteration.
+
 a=1
 while True:
     print(a)
