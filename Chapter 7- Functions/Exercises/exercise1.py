ec92c1030a5b5d9ac03910fb9a9bda845f63f273
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:45:35 +0400

    Update exercise1.py

--- Chapter 7- Functions/Exercises/exercise1.py
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -1,3 +1,6 @@
+#Define a function called 'display_message' that displays a message about what the user is learning.
+#Inside the function, create a message and print it.
+# Call the 'display_message' function to execute it and display the message.
 def display_message():
     """Display a message about what I'm learning."""
     msg = "I'm learning about how to store code in functions."
