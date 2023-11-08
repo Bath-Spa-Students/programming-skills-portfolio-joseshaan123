d9c041c38e577068534dc6e42f4476f3fb949e06
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:37:20 +0400

    Update exercise2.py

--- Chapter 5- Dictionaries/Exercises/exercise2.py
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -1,11 +1,13 @@
+# Create a dictionary called 'glossary' with key-value pairs, defining terms and their explanations.
 glossary = {
-    'string': 'A series of characters.',
-    'comment': 'the lines in the code that are ignored by the interpreter during the execution of the program.',
-    'list': 'a data structure in Python that is a mutable, or changeable.',
-    'loop': 'repeating something over and over until a particular condition is satisfied..',
-    'dictionary': " It consists of a collection of key-value pairs.",
-    }
+    'string': 'They are a series of characters.',
+    'comment': 'The lines in the code that are ignored by the interpreter during the execution of the program.',
+    'list': 'A data structure in Python that is mutable, or changeable.',
+    'loop': 'Repeating something over and over until a particular condition is satisfied.',
+    'dictionary': 'It consists of a collection of key-value pairs.',
+}
 
+# Print the explanations for specific terms from the 'glossary' dictionary using their respective keys.
 word = 'string'
 print(f"\n{word.title()}: {glossary[word]}")
 
