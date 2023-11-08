88671ede039b0dd9fe8cfc95c6247d9ce04da3d7
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:21:47 +0400

    Update exercise4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -1,5 +1,10 @@
-string1=str("shaan ")
-string2=str("jose ")
-string3=str("baiju ")
-string= string1 + string2+ string3
+ # Create three string variables 'string1', 'string2', and 'string3' and assign them specific values.
+string1 = str("shaan ")  # Assign the value "shaan " to 'string1'.
+string2 = str("jose ")   # Assign the value "jose " to 'string2'.
+string3 = str("baiju ")  # Assign the value "baiju " to 'string3'.
+
+# Concatenate the three string variables 'string1', 'string2', and 'string3' to create a new string called 'string'.
+string = string1 + string2 + string3  # Combine the three strings together.
+
+# Print the resulting 'string' to the console, which will contain the concatenated values of 'string1', 'string2', and 'string3'.
 print(string)
