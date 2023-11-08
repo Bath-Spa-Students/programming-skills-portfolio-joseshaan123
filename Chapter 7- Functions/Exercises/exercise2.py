003e3b59a4b61999eea28dfd849dd45368cc9819
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:46:16 +0400

    Update exercise2.py

--- Chapter 7- Functions/Exercises/exercise2.py
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -1,4 +1,7 @@
+# Define a function called favorite_book that accepts a title parameter
+# Print a message with the provided book title
 def favorite_book(title):
     print(f"{title} is one of my favorite story books.")
 
+# Call the function with your favorite book title
 favorite_book('The Jungle Book')
