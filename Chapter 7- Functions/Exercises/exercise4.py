18c6158e4ef1f229c338e40093b0684a09894050
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 19:39:44 +0400

    Update exercise4.py

--- Chapter 7- Functions/Exercises/exercise4.py
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -2,7 +2,9 @@ def make_shirt(size='large', message='I love Python!'):
     """Summarize the shirt that's going to be made."""
     print(f"\nI'm going to make a {size} t-shirt.")
     print(f'It will say, "{message}"')
-
+#Make a large shirt with the default message.
 make_shirt()
+#Make a medium shirt with the default message
 make_shirt(size='medium')
+#Make a shirt of any  size with a different message
 make_shirt('small', 'Day and Night.')
