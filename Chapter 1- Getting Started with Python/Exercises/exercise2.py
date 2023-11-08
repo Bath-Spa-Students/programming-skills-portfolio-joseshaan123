82e8af3c37e8412b623ed3b410b8d8fc7e339f53
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:19:48 +0400

    Update exercise2.py

--- Chapter 1- Getting Started with Python/Exercises/exercise2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise2.py
@@ -1,5 +1,10 @@
+# Import the 'sys' module, which provides access to some variables used or maintained by the interpreter.
 import sys
+# Print the string "Python version" to indicate that we are about to display the Python version.
 print("python.version")
+# Using the 'sys.version' attribute to obtain and print the Python version information.
 print(sys.version)
+# Print the string "Version info" to indicate that we are about to display version information.
 print("verison_info")
+# Use the 'sys.version_info' attribute to obtain and print detailed version information about Python.
 print(sys.version_info)
