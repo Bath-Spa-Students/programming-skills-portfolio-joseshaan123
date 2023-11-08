f158436359be74ce1599c643959b171b4761f29e
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 19:40:21 +0400

    Update exercise5.py

--- Chapter 7- Functions/Exercises/exercise5.py
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -1,8 +1,10 @@
+#Defining a function
 def describe_city(city, country='India'):
     """Describe a city."""
     msg = f"{city.title()} is in {country.title()}."
     print(msg)
 
+# Call the function for three different cities
 describe_city('Kerala')
 describe_city('Lahore', 'Pakistan')
-describe_city('Tamil Nadu')
+describe_city('Tamil Nadu') #This will use new default country value
