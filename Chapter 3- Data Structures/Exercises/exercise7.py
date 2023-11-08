297e64f2647030fb38cfbf0a83387d0aebe8afaf
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:31:46 +0400

    Update exercise7.py

--- Chapter 3- Data Structures/Exercises/exercise7.py
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -1,31 +1,42 @@
-locations=['Kerala','Karnataka','Tamil Nadu','Delhi']
+# Create a list called 'locations' containing four place names.
+locations = ['Kerala', 'Karnataka', 'Tamil Nadu', 'Delhi']
+
+# Print the original order of the list.
 print("Original order:")
 print(locations)
 
+# Print the list in alphabetical order without modifying the original list.
 print("\nAlphabetical:")
 print(sorted(locations))
 
+# Print the original order of the list (no changes made to the original list).
 print("\nOriginal order:")
 print(locations)
 
+# Print the list in reverse alphabetical order without modifying the original list.
 print("\nReverse alphabetical:")
 print(sorted(locations, reverse=True))
 
+# Print the original order of the list (no changes made to the original list).
 print("\nOriginal order:")
 print(locations)
 
+# Reverse the order of the list in place.
 print("\nReversed:")
 locations.reverse()
 print(locations)
 
+# Print the original order of the list.
 print("\nOriginal order:")
 locations.reverse()
 print(locations)
 
-print("\nAlphabetical")
+# Sort the list in alphabetical order in place.
+print("\nAlphabetical:")
 locations.sort()
 print(locations)
 
-print("\nReverse alphabetical")
+# Sort the list in reverse alphabetical order in place.
+print("\nReverse alphabetical:")
 locations.sort(reverse=True)
 print(locations)
