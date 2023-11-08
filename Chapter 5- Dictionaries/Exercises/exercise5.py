63d5b4b8e154217948709500c3b29dece4b7c010
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:41:00 +0400

    Update exercise5.py

--- Chapter 5- Dictionaries/Exercises/exercise5.py
+++ Chapter 5- Dictionaries/Exercises/exercise5.py
@@ -1,3 +1,7 @@
+# Create an empty list called 'pets' to store information about pets.
+
+# Define a dictionary 'pet' with information about a pet and append it to the 'pets' list.
+
 pets=[]
 
 pet = {
@@ -9,6 +13,7 @@ pet = {
 }
 pets.append(pet)
 
+# Define another dictionary 'pet' with information about a different pet and append it to the 'pets' list.
 pet = {
     'Species': 'bird',
     'name': 'Annie',
@@ -18,6 +23,7 @@ pet = {
 }
 pets.append(pet)
 
+# Define one more dictionary 'pet' with information about another pet and append it to the 'pets' list.
 pet = {
     'Species': 'Mammal',
     'name': 'Negan',
@@ -27,6 +33,7 @@ pet = {
 }
 pets.append(pet)
 
+# Use a for loop to iterate through the 'pets' list and print information about each pet.
 for pet in pets:
     print(f"\nThings I know about {pet['name'].title()}:")
     for key, value in pet.items():
