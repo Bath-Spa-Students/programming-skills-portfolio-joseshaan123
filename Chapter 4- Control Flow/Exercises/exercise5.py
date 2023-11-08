f134ba87cc59d3152f5ea6883a66bd55110fbb6b
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:35:32 +0400

    Update exercise5.py

--- Chapter 4- Control Flow/Exercises/exercise5.py
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -10,3 +10,26 @@ if 'Avocado' in fruits:
  print("I like Avocado")
 if 'Pomegranate' in fruits:
  print(" I like Pomegranate")
+
+# Create a list called 'fruits' containing three fruit names.
+fruits = ['Apple', 'Mango', 'Orange']
+
+# Check if 'Apple' is in the 'fruits' list and print a message if it is.
+if 'Apple' in fruits:
+    print("I like Apples")
+
+# Check if 'Grapes' is in the 'fruits' list (which it's not) and do not print the message.
+if 'Grapes' in fruits:
+    print("I like Grapes")
+
+# Check if 'Orange' is in the 'fruits' list and print a message if it is.
+if 'Orange' in fruits:
+    print("I like Orange")
+
+# Check if 'Avocado' is in the 'fruits' list (which it's not) and do not print the message.
+if 'Avocado' in fruits:
+    print("I like Avocado")
+
+# Check if 'Pomegranate' is in the 'fruits' list (which it's not) and do not print the message.
+if 'Pomegranate' in fruits:
+    print("I like Pomegranate")
