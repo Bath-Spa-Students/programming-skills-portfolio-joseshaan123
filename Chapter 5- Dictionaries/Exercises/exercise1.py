a0911b1cf13f0dfbc34c338e39c0f7628d18967e
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-08 18:36:39 +0400

    Update exercise1.py

--- Chapter 5- Dictionaries/Exercises/exercise1.py
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -1,10 +1,12 @@
+# Create a dictionary called 'person' with key-value pairs for personal information.
 person = {
     'first_name': 'Shaan',
     'last_name': 'Jose',
     'age': 18,
     'city': 'Sharjah',
-    }
+}
 
+# Print the values associated with specific keys in the 'person' dictionary.
 print(person['first_name'])
 print(person['last_name'])
 print(person['age'])
