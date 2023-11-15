504771758f05c010c5ceb0c2d58f1f5d8cec27c3
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-15 23:54:52 +0400

    Update exercise3.py

--- Chapter 7- Functions/Exercises/exercise3.py
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -1,8 +1,8 @@
 #Defining a function 
 def make_shirt(size, message):
-    """Summarize the shirt that's going to be made."""
+    """Reflect about the shirt that's going to be made."""
     print(f"\nI'm going to make a {size} t-shirt.")
-    print(f'It will say, "{message}"')
+    print(f'It will show, "{message}"')
 
 # Call the Make shirt function with specific size and message arguements.
 make_shirt('large', 'Night life')
