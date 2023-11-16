a6fa899d6106daefb230a85928ba47cc0cc41194
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2023-11-16 12:24:20 +0400

    Update exercise4.py

--- Chapter 7- Functions/Exercises/exercise4.py
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -1,5 +1,5 @@
 def make_shirt(size='large', message='I love Python!'):
-    """Reflect about the shirt that's going to be made."""
+    """Reflect on the shirt that's going to be made."""
     print(f"\nI'm going to make a {size} t-shirt.")
     print(f'It will show, "{message}"')
 #Make a large shirt with the default message.
